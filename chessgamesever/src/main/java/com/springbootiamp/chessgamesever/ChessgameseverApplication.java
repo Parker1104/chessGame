0d39c32b3b118b43ee7c0c2f9a5165aa8bf3f029
Parker1104 <16937688@qq.com>
2018-12-28 17:38:06 +0800

    create chessGame project

---
+++ chessgamesever/src/main/java/com/springbootiamp/chessgamesever/ChessgameseverApplication.java
@@ -0,0 +1,14 @@
+package com.springbootiamp.chessgamesever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessgameseverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessgameseverApplication.class, args);
+	}
+
+}
+
